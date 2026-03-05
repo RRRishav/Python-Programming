620668bb420fde78bbd5602f56ac8fe3a3a0a9e0
RRRishav <rishav6787@gmail.com>
2026-03-06 01:05:29 +0530

    load

---
+++ operator-overload.py
@@ -0,0 +1,20 @@
+class Number:
+
+
+  def __init__(self,num):
+    self.num = num
+
+
+  def __add__(self,num2):
+    print("add")
+    return self.num  + num2.num
+
+
+
+n1 = Number(7)
+n2 = Number(7)
+
+
+sum = n1+n2
+
+print(sum)
--- property_decorator.py
+++ property_decorator.py
@@ -9,7 +9,15 @@ class Employee:
   def totalSal(self):
     return self.salary + self.salbonus
 
+  @totalSal.setter
+
+  def totalSal(self,val):
+    self.salbonus  = val  - self.salary
+
+
 
 e = Employee()
 print(e.totalSal)
-
+e.totalSal = 56
+print(e.salary)
+print(e.salbonus)
