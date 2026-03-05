833ede2cfa12bfb6901867475e02cace55762272
RRRishav <rishav6787@gmail.com>
2026-03-06 01:42:41 +0530

    A

---
+++ practice_inheritance.py
@@ -0,0 +1,42 @@
+# class C2DVector:
+#   def __init__(self,i,j):
+#     self.icap = i
+#     self.jcap = j
+
+#   def __str__(self):
+#     return f"{self.icap}i +  {self.jcap}j"
+
+# class T3DVec(C2DVector):
+#   def __init__(self,i,j,k):
+#     super().__init__(i,j)
+#     self.kcap = k
+
+#   def __str__(self):
+#     return f"{self.icap}i +  {self.jcap}j +{self.kcap}k"
+
+
+# v2d = C2DVector(1,3)
+# v3d = T3DVec(7,7,7)
+
+# print(v2d)
+# print(v3d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
