4deeeee08ce4f759c981eaeddf3ebcb1e0363c2f
RRRishav <rishav6787@gmail.com>
2026-03-07 09:42:48 +0530

    ok

---
+++ oops.more.py
@@ -0,0 +1,60 @@
+# class Student:#creating class
+#   name = "Rishav"
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+
+
+
+# class Car:
+#   color= "black"
+
+
+# car1 = Car()
+# print(car1.color)
+
+
+
+
+
+
+
+
+
+
+#constructor object creation
+
+
+# class Student:
+
+#   def __init__(self):
+#     pass
+
+
+
+
+#   def __init__(self,fname,love,place):
+#     self.fname = fname
+#     self.love = love
+#     self.place = place
+#     #print(self)
+#     print(f"creating new weeding {fname} with loves {love} and place is {place}")
+
+# s1 = Student("Rishav","Akanksha sharma","Banglore")
+# print(s1.fname)
+
+
+
+
+
+
+
+
+
+
+
+
