c4256b8b60e555a866c894f5c3d96ebea2b24d6f
RRRishav <rishav6787@gmail.com>
2026-03-07 10:15:51 +0530

    ok

--- oops.more.py
+++ oops.more.py
@@ -63,27 +63,109 @@
 
 
 
-class Student:
+# class Student:
 
-  def __init__(self,name1,name2,name3,mark1,mark2,mark3):
-    self.name1 = name1
-    self.name2 = name2
-    self.name3 = name3
-    self.mark1 = mark1
-    self.mark2 = mark2
-    self.mark3 = mark3
+#   def __init__(self,name1,name2,name3,mark1,mark2,mark3):
+#     self.name1 = name1
+#     self.name2 = name2
+#     self.name3 = name3
+#     self.mark1 = mark1
+#     self.mark2 = mark2
+#     self.mark3 = mark3
 
-  def getDeatails(self):
+#   def getDeatails(self):
 
-    print(f"details are {self.name1}, {self.name2} {self.name3}")
+#     print(f"details are {self.name1}, {self.name2} {self.name3}")
 
-  def marksAVG(self):
-    return (self.mark1+ self.mark2 + self.mark3)/300 * 100
+#   def marksAVG(self):
+#     return (self.mark1+ self.mark2 + self.mark3)/300 * 100
 
 
-s1 = Student("Rishav","Akanksha","Harsh",91,23,43)
-s1.getDeatails()
-print(s1.marksAVG())
+# s1 = Student("Rishav","Akanksha","Harsh",91,23,43)
+# s1.getDeatails()
+# print(s1.marksAVG())
+
+
+
+
+
+
+# #ABSTRACTION
+# class Car:
+
+#   def __init__(self):
+#     self.acc = False
+#     self.brek= False
+#     self.cltch = False
+
+#   def start(self):
+#     self.cltch = True
+#     self.brek = True
+#     self.acc = True
+#     print(" Car is started")
+
+# car = Car()
+# car.start()
+
+
+
+
+
+
+#Encapsulation
+
+
+# class Account:
+#   # balance = 1000000
+#   # Acc_no= 234344545545
+
+#   def __init__(self,acc,bal):
+#     self.bal = bal
+#     self.Acc_no = acc
+
+#   def debit(self,amount):
+#     self.bal = self.bal - amount
+#     print("Rs.",amount,"was debited")
+
+#   def debit(self,amount):
+#     self.bal = self.bal + amount
+#     print("Rs.",amount,"was debited")
+
+
+
+
+# acc1 = Account(100000,1232333434)
+# acc1.debit(4000)
+# print(acc1.bal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
