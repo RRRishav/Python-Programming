f59bd43519b074711b9b6502184c186f79e9f900
RRRishav <rishav6787@gmail.com>
2026-03-07 10:03:19 +0530

    more abouts oop

--- oops.more.py
+++ oops.more.py
@@ -31,28 +31,59 @@
 
 # class Student:
 
+#   collegeName = "TIET PATIALA"
+#   #name = "anony"
 #   def __init__(self):
 #     pass
 
-
-
-
 #   def __init__(self,fname,love,place):
-#     self.fname = fname
+#     self.fname = fname #obj attr > class attr
 #     self.love = love
 #     self.place = place
 #     #print(self)
 #     print(f"creating new weeding {fname} with loves {love} and place is {place}")
 
+#   @staticmethod
+#   def hello():
+#     print("hello")
+
+#   def welcome(self):
+#     print(f"Congratulations to {self.fname} and {self.love}")
+
+#   def palce(self):
+#     return self.place
+
 # s1 = Student("Rishav","Akanksha sharma","Banglore")
 # print(s1.fname)
+# s1.hello()
+# s1.welcome()
+# print(s1.palce())
+# print(s1.collegeName)
 
 
 
 
+class Student:
+
+  def __init__(self,name1,name2,name3,mark1,mark2,mark3):
+    self.name1 = name1
+    self.name2 = name2
+    self.name3 = name3
+    self.mark1 = mark1
+    self.mark2 = mark2
+    self.mark3 = mark3
+
+  def getDeatails(self):
+
+    print(f"details are {self.name1}, {self.name2} {self.name3}")
+
+  def marksAVG(self):
+    return (self.mark1+ self.mark2 + self.mark3)/300 * 100
 
 
-
+s1 = Student("Rishav","Akanksha","Harsh",91,23,43)
+s1.getDeatails()
+print(s1.marksAVG())
 
 
 
