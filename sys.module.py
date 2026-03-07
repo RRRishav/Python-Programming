2351fbfebd575a5fee772e0b39dee3a1c34dd4e3
RRRishav <rishav6787@gmail.com>
2026-03-07 14:08:06 +0530

    sys module

---
+++ sys.module.py
@@ -0,0 +1,28 @@
+# import sys
+
+# print("Python version")
+# print(sys.version)
+
+
+#2. Print Command Line Arguments
+
+
+# import sys
+# print("command line arguments")
+
+# for arg in sys.argv:
+#   print(arg)
+
+
+
+#3 Count Command Line Arguments
+
+
+
+# import sys
+
+# print("Total arguments:", len(sys.argv))
+
+
+# import sys
+# print(sys.getsizeof(10))
